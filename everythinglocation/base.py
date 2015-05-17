a0ae7d78d8b2a495efdc0a506a872cef3cec3aba
Justin Cano <jcano001@ucr.edu>
2015-05-16 21:22:46 -0700

    removed unnecessary method

--- everythinglocation/base.py
+++ everythinglocation/base.py
@@ -66,16 +66,3 @@ class EverythingLocation(object):
 
     def _DELETE(self, path, params=None, payload=None):
         return self._request('DELETE', path, params=params, payload=payload)
-
-    def _set_attrs_to_values(self, response={}):
-        """
-        Set attributes to dictionary values.
-        - e.g.
-        >>> import tmdbsimple as tmdb
-        >>> movie = tmdb.Movies(103332)
-        >>> response = movie.info()
-        >>> movie.title  # instead of response['title']
-        """
-        if isinstance(response, dict):
-            for key in response.keys():
-                setattr(self, key, response[key])
