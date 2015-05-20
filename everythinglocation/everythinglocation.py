08c41a15156cff75167f502f596e500d09f851cc
Justin Cano <jcano001@ucr.edu>
2015-05-20 16:39:03 -0700

    created EverythingLocation class and updated unit tests

---
+++ everythinglocation/everythinglocation.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+everythinglocation.everythinglocation
+~~~~~~~~~~~~~~~
+This module implements the class that makes calls to the everythinglocation API.
+
+:copyright: (c) 2015 by Justin Cano
+:license: MIT, see LICENSE for more details
+"""
+import json, requests, os
+
+from .base import Loqate
+from .response import ELResponse
+
+class APIKeyError(Exception):
+    pass
+
+class EverythingLocation(Loqate):
+    BASE_PATH = 'rest'
+    RESOURCES = {
+        'version': 'version'
+    }
+
+    def __init__(self):
+        super(EverythingLocation, self).__init__()
+        self.response = ELResponse(self._GET(resource='version'))
+        self.version = self.response.version
+
+    def verify(self, params):
+        params['p'] = 'v'
+        return ELResponse(self._GET(params=params))
+
